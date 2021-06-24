3328be487f27264a5f9013b9690ceaebe099f32b
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2021-06-24 10:07:45 +0100

    changing feature in server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js'
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 
-// 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-app.get('/', (req, res) => {
-    res.send("API is running");
-})
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.get('/api/products', (req, res) => {
     res.json(products);
